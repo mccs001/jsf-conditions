10d32fd85d0dc3f4859575cf512421214aff2c29
Stephen McCallum <stephen_mccallum@yahoo.com>
2024-09-29 14:57:49 +0100

    Update code to read temperatures from BME280

--- device_code/src/OurSensors.cpp
+++ device_code/src/OurSensors.cpp
@@ -1,38 +1,92 @@
 #include <Arduino.h>
 #include "OurSensors.h"
+#include <Wire.h>
+#include "BlueDot_BME280.h"
 
-/* placeholder implementation using random numbers until actually getting sensors working */
+BlueDot_BME280 bme1; // Object for Sensor 1    outside
+BlueDot_BME280 bme2; // Object for Sensor 2    inside
 
-static double s_currentInnerTemp = 20.0;
-static double s_currentOuterTemp = 12.0;
-
+int bme1Detected = 0;
+int bme2Detected = 0;
 
 OurSensors::OurSensors()
 {
-  // this will be called when the object is constructed (before the Arduino setup function is called)
-  randomSeed(analogRead(0));
-
 }
 
 void OurSensors::onSetup()
 {
   // this will be called at setup time
+  Wire.begin(43, 44);
 
+  bme1.parameter.communication = 0; // I2C communication for Sensor 1 (bme1)
+  bme2.parameter.communication = 0; // I2C communication for Sensor 2 (bme2)
+
+  bme1.parameter.I2CAddress = 0x77; // I2C Address for Sensor 1 (bme1)
+  bme2.parameter.I2CAddress = 0x76; // I2C Address for Sensor 2 (bme2)
+
+  bme1.parameter.sensorMode = 0b11; // Setup Sensor mode for Sensor 1
+  bme2.parameter.sensorMode = 0b11; // Setup Sensor mode for Sensor 2
+
+  bme1.parameter.IIRfilter = 0b100; // IIR Filter for Sensor 1
+  bme2.parameter.IIRfilter = 0b100; // IIR Filter for Sensor 2
+
+  bme1.parameter.humidOversampling = 0b101; // Humidity Oversampling for Sensor 1
+  bme2.parameter.humidOversampling = 0b101; // Humidity Oversampling for Sensor 2
+
+  bme1.parameter.tempOversampling = 0b101; // Temperature Oversampling for Sensor 1
+  bme2.parameter.tempOversampling = 0b101; // Temperature Oversampling for Sensor 2
+
+  bme1.parameter.pressOversampling = 0b101; // Pressure Oversampling for Sensor 1
+  bme2.parameter.pressOversampling = 0b101; // Pressure Oversampling for Sensor 2
+
+  bme1.parameter.pressureSeaLevel = 1013.25; // default value of 1013.25 hPa (Sensor 1)
+  bme2.parameter.pressureSeaLevel = 1013.25; // default value of 1013.25 hPa (Sensor 2)
+
+  bme1.parameter.tempOutsideCelsius = 15; // default value of 15°C
+  bme2.parameter.tempOutsideCelsius = 15; // default value of 15°C
+
+  bme1.parameter.tempOutsideFahrenheit = 59; // default value of 59°F
+  bme2.parameter.tempOutsideFahrenheit = 59; // default value of 59°F
+
+  //*************ADVANCED SETUP IS OVER - LET'S CHECK THE CHIP ID!*******
+
+  if (bme1.init() != 0x60)
+  {
+    Serial.println("Oops! First BME280 Sensor not found!");
+    bme1Detected = 0;
+  }
+
+  else
+  {
+    Serial.println("First  BME280 Sensor detected!");
+    bme1Detected = 1;
+  }
+
+  if (bme2.init() != 0x60)
+  {
+    Serial.println("Oops! Second BME280 Sensor not found!");
+    bme2Detected = 0;
+  }
+
+  else
+  {
+    Serial.println("Second BME280 Sensor detected!");
+    bme2Detected = 1;
+  }
 }
 
 static double getRandomTempChange()
 {
   // this returns a random number somewhere between -1.0 and 1.0 (in steps of 0.1)
-  return ((double) (random(21) - 10)) / 10.0;
+  return ((double)(random(21) - 10)) / 10.0;
 }
 
-bool OurSensors::tryToReadSensors(double& insideTemperature, double& outsideTemperature)
+bool OurSensors::tryToReadSensors(double &insideTemperature, double &outsideTemperature)
 {
-  s_currentInnerTemp += getRandomTempChange();
-  s_currentOuterTemp += getRandomTempChange();
+  // should really check bme1Detected and bme2Detected ...
 
-  insideTemperature = s_currentInnerTemp;
-  outsideTemperature = s_currentOuterTemp;
+  insideTemperature = bme2.readTempC();
+  outsideTemperature = bme1.readTempC();
 
   return true;
 }
